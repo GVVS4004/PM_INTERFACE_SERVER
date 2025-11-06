57b3334defcc76569d8f0fc4ffc7879bfc05d5ba
Vijay Gourabattuni <Vijay.Gourabattuni@keyloop.com>
2025-11-06 21:45:06 +0530

    cors fixed

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: 'https://pm-interface-wpgt-83r2nv0uj-gvijay4004gmailcoms-projects.vercel.app', methods: ['GET', 'POST', 'PUT', 'DELETE'], credentials: true }));
+app.use(cors({
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
