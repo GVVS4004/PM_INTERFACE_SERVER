08bd4da36a8dc48fe0b9dd29e1ac8d799d544ca9
Vijay Gourabattuni <Vijay.Gourabattuni@keyloop.com>
2025-11-06 21:23:29 +0530

    added origin

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: '*', credentials: true }));
+app.use(cors({ origin: 'https://pm-interface-wpgt-83r2nv0uj-gvijay4004gmailcoms-projects.vercel.app', methods: ['GET', 'POST', 'PUT', 'DELETE'], credentials: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
